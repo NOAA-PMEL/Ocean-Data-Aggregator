44808683fe5098edb60e92ebad5e197fce3488a5
zalmanek <46435419+brynnz22@users.noreply.github.com>
2025-08-29 10:53:45 -0700

    Add ocean model conversion functionality and units

--- utils/mooring_aggregator.py
+++ utils/mooring_aggregator.py
@@ -1,5 +1,7 @@
 from utils.aggregator import Aggregator
 from utils.mat_file_processor import MatFileProcessor
+from utils.netcdf_processor import NetcdfProcessor
+from pathlib import Path
 import pandas as pd
 
 # TODO: upate hardcoded sites in the convert_mat_files_to_dfs to not be hardcoded (take from Quagmire sites)
@@ -13,13 +15,52 @@ class MooringAggregator(Aggregator):
         # Config file info
         self.mooring_mat_files = self.config_file['mooring_mat_files']
         self.quagmire_site_col_name = self.config_file['quagmire_info']['site_col_name']
+        self.quagmire_depth_col_name = self.config_file['quagmire_info']['depth_col_name']
+        self.quagmire_date_col_name = self.config_file['quagmire_info']['date_col_name']
+        self.ctd_nc_file_directory = Path(
+            self.config_file['ctd_data']['net_cdf_dir'])
+        self.model_data_files = self.config_file['ocean_model_data']['model_nc_files']
+        self.ocean_model_depth_var = self.config_file['ocean_model_data']['depth_variable_name']
+        self.ocean_model_vert_dim_name = self.config_file['ocean_model_data']['vertical_dim_name']
+        self.ocean_model_time_dim_name = self.config_file['ocean_model_data']['time_dim_name']
 
         # Deduced
         self.mooring_sites = self.quagmire_df[self.quagmire_site_col_name].unique(
         ).tolist()
-        self.mooring_df = self.convert_mat_files_to_dfs()
+        self.min_date, self.max_date = self.get_quag_min_and_max_dates()
+        self.min_depth, self.max_depth = self.get_min_and_max_depths()
+        # self.mooring_df = self.convert_mat_files_to_df()
+        # self.ctd_from_netcdf_df = self.convert_ctd_nc_files_to_df()
+        self.ocean_model_df = self.convert_ocean_model_nc_to_df()
 
-    def convert_mat_files_to_dfs(self) -> pd.DataFrame:
+    def get_quag_min_and_max_dates(self) -> tuple:
+        """
+        Get the min and max dates from the Quagmire - to plug into ocean model data query
+        """
+        self.quagmire_df[self.quagmire_date_col_name] = pd.to_datetime(
+            self.quagmire_df[self.quagmire_date_col_name])
+
+        min_date = self.quagmire_df[self.quagmire_date_col_name].min()
+        max_date = self.quagmire_df[self.quagmire_date_col_name].max()
+
+        # Format the dates into YYYY-MM-DD and YYYY-MM strings
+        min_date_full_format = min_date.strftime('%Y-%m-%d')
+        max_date_full_format = max_date.strftime('%Y-%m-%d')
+
+        return min_date_full_format, max_date_full_format
+
+    def get_min_and_max_depths(self) -> tuple:
+        """
+        Get the min and max depths from the quagmire - to get range of dates to query ocean model data
+        """
+
+        # Get min and max depths from quagmire
+        min_depth = self.quagmire_df[self.quagmire_depth_col_name].min()
+        max_depth = self.quagmire_df[self.quagmire_depth_col_name].max()
+
+        return min_depth, max_depth
+
+    def convert_mat_files_to_df(self) -> pd.DataFrame:
         # TODO: upate hardcoded sites in the convert_mat_files_to_dfs to not be hardcoded (take from Quagmire sites)
         """
         Converts the mooring .mat files associated with the Aggregator to pandas
@@ -35,3 +76,40 @@ class MooringAggregator(Aggregator):
             mooring_dfs.append(mooring_df)
 
         return pd.concat(mooring_dfs, ignore_index=True)
+
+    def convert_ctd_nc_files_to_df(self) -> pd.DataFrame:
+        """
+        Converts all the associated .nc files in the config.yaml into a data frame. Concats them all
+        together to return one dataframe. Assumes that ctd files are all in the same directory.
+        """
+        # Recurseivly find all .nc files in the directory
+        all_nc_files = list(self.ctd_nc_file_directory.rglob('*.nc'))
+
+        # Filter the list of all_nc_files based on the station_ids
+        nc_files_needed = [
+            f for f in all_nc_files if any(station_id in str(f) for station_id in self.mooring_sites)
+        ]
+
+        nc_dfs = []
+        for nc_file in nc_files_needed:
+            nc_processor = NetcdfProcessor(nc_file=nc_file)
+            nc_df = nc_processor.convert_ctd_nc_to_df()
+            nc_dfs.append(nc_df)
+
+        return pd.concat(nc_dfs, ignore_index=True)
+
+    def convert_ocean_model_nc_to_df(self) -> pd.DataFrame:
+
+        nc_dfs = []
+        for nc_file in self.model_data_files:
+            nc_processor = NetcdfProcessor(nc_file=nc_file)
+            nc_df = nc_processor.convert_rom_ocean_model_to_df(min_depth=self.min_depth,
+                                                               max_depth=self.max_depth,
+                                                               depth_var_name=self.ocean_model_depth_var,
+                                                               vertical_dim_name=self.ocean_model_vert_dim_name,
+                                                               time_dim_name=self.ocean_model_time_dim_name,
+                                                               start_time=self.min_date,
+                                                               end_time=self.max_date)
+            nc_dfs.append(nc_df)
+
+        return pd.concat(nc_dfs, ignore_index=True)
--- utils/netcdf_processor.py
+++ utils/netcdf_processor.py
@@ -9,9 +9,9 @@ class NetcdfProcessor:
 
         self.nc_file = Path(nc_file)
 
-    def convert_nc_to_df(self):
+    def convert_ctd_nc_to_df(self):
         """
-        Concerts a netCDF file to a dataframe. Extracts the date/time and station_id from the file name
+        Concerts a CTD netCDF file to a dataframe. Extracts the date/time and station_id from the file name
         """
         with xr.open_dataset(self.nc_file) as nc_file:
             nc_df = nc_file.to_dataframe()
@@ -29,3 +29,71 @@ class NetcdfProcessor:
             nc_df.loc[:, 'date'] = date
 
             return nc_df
+
+    def convert_rom_ocean_model_to_df(self, min_depth: float, max_depth: float,
+                                      depth_var_name: str, vertical_dim_name: str,
+                                      time_dim_name: str,
+                                      start_time: str,
+                                      end_time: str) -> pd.DataFrame:
+        """
+        Extracts time and depth-averages data from a NetCDF file, returning a pandas DataFrame
+        """
+        ds = xr.open_dataset(self.nc_file)
+
+        # 1. Filter by the specified time range, if provided
+        ds = ds.sel({time_dim_name: slice(start_time, end_time)})
+
+        # 2. Identify variables to process based on their dimension
+        variables_to_avg = [
+            var for var in ds.data_vars
+            if vertical_dim_name in ds[var].dims and time_dim_name in ds[var].dims
+        ]
+
+        # 3. Identify 1D variables that should be included
+        variables_to_keep = [
+            var for var in ds.data_vars
+            if vertical_dim_name not in ds[var].dims and time_dim_name in ds[var].dims
+        ]
+
+        # 4. Filter the dataset by the specified depth range.
+        ds_filtered_by_depth = ds.where(
+            (ds[depth_var_name] >= min_depth) & (
+                ds[depth_var_name] <= max_depth),
+            drop=False
+        )
+
+        # 5. Calculate the mean of each variable over the vertical dimension.
+        ds_averaged = ds_filtered_by_depth[variables_to_avg].mean(
+            dim=vertical_dim_name, skipna=True
+        )
+
+        # 6. Add the 1D variables back to the averaged dataset.
+        for var in variables_to_keep:
+            ds_averaged[var] = ds[var]
+
+        # Get units from original dataset
+        new_col_name_dict = self.get_units_from_nc_vars(
+            avg_xr_ds=ds_averaged, original_xr_ds=ds)
+
+        ds_final = ds_averaged.rename(new_col_name_dict)
+
+        return ds_final.to_dataframe()
+
+    def get_units_from_nc_vars(self, avg_xr_ds: xr, original_xr_ds) -> dict:
+        # Gets the units from the xr_dataset vars and returns a dictionary with var: new_name (includes units)
+        # avg_xr_ds and original_xr_ds will be the same for one dimensional data like ctd data, otherwise different
+        # for ocean model data
+        rename_dict = {}
+        for var in list(avg_xr_ds.data_vars):
+            units = original_xr_ds[var].attrs.get('units', 'no_units')
+
+            # Check if the units are actually present and not an empty string
+            if units:
+                new_name = f"{var}_{units}".replace(
+                    ' ', '_').replace('/', '_per_')
+                rename_dict[var] = new_name
+            else:
+                # If no units, just keep the original name
+                rename_dict[var] = var
+
+        return rename_dict
