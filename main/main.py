35ef248b7228b4fc3c3167cae1634574b7504371
zalmanek <46435419+brynnz22@users.noreply.github.com>
2025-08-29 11:34:04 -0700

    update main

--- main/main.py
+++ main/main.py
@@ -1,9 +1,9 @@
 from utils.mooring_aggregator import MooringAggregator
 
+
 # python -m main.main
 
 mooring_agg = MooringAggregator(
     config_yaml='/Users/zalmanek/Development/Ocean-Data-Aggregator/main/config.yaml')
 
-mooring_df = mooring_agg.convert_mat_files_to_dfs()
-print(mooring_df)
+mooring_agg.mooring_df.to_csv('mooring_data.csv')
